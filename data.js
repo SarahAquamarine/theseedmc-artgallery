e9ab61bce21ec81139431d955a09dacfa4d739dd
SarahAzureHeart <106204859+SarahAquamarine@users.noreply.github.com>
2026-04-04 14:03:53 -0700

    Update data.js

--- data.js
+++ data.js
@@ -100,6 +100,17 @@ const maparts = [
   status: "expired",
   transparent: true
 },
+{
+  image: "images/adam-smasher.png",
+  title: "Adam Smasher",
+  artist: "Lenalovescows & unknownPLAY3R",
+  warp: "/warp mapart by lena",
+  size: "4x2",
+  dateAdded: "2026-04-03",
+  categories: ["Gaming", "Sci-Fi", "Characters"],
+  status: "expired",
+  transparent: true
+},
   // ==========================
   // ===== Pokemon ===============
   // ==========================
