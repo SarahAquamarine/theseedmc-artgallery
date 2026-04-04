63772b4a9033c011abe6dbd4af42125f160e84d5
SarahAzureHeart <106204859+SarahAquamarine@users.noreply.github.com>
2026-04-03 18:46:46 -0700

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,36 @@ const maparts = [
   status: "expired",
   transparent: true
   },
-
+  {
+  image: "images/mikasa.png",
+  title: "Mikasa",
+  artist: "Lenalovescows & unknownPLAY3R",
+  warp: "/warp mapart by lena",
+  size: "2x3",
+  dateAdded: "2026-04-03",
+  categories: ["Anime", "Characters"],
+  transparent: true
+},
+{
+  image: "images/power.png",
+  title: "Power",
+  artist: "Lenalovescows & unknownPLAY3R",
+  warp: "/warp mapart by lena",
+  size: "2x3",
+  dateAdded: "2026-04-03",
+  categories: ["Anime", "Characters"],
+  transparent: true
+},
+{
+  image: "images/storm-trooper.png",
+  title: "Stormtrooper",
+  artist: "Lenalovescows & unknownPLAY3R",
+  warp: "/warp mapart by lena",
+  size: "2x3",
+  dateAdded: "2026-04-03",
+  categories: ["Sci-Fi", "Characters"],
+  transparent: true
+},
   // ==========================
   // ===== Pokemon ===============
   // ==========================
